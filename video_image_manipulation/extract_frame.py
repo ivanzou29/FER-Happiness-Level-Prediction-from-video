bb11ab3958e95360aa56b54ef4f2fe0079562180
ivanrz29 <ivanzou29@gmail.com>
2019-11-06 16:48:20 +0800

    add extract frame script

---
+++ video_image_manipulation/extract_frame.py
@@ -0,0 +1,12 @@
+import os
+
+def extract_frames(path, filename):
+    os.system("mkdir {0}".format(filename[:12]))
+    os.system("ffmpeg -i {0} -f image2 -vf fps=fps=1 {1}/output%d.png".format(os.path.join(path, filename), filename[:12]))
+
+paths = ['Happy', 'Neutral', 'Sad']
+if __name__ == '__main__':
+    for path in paths:
+        for filename in os.listdir(path):
+            if (filename.endswith(".avi")):
+                extract_frames(path, filename)
