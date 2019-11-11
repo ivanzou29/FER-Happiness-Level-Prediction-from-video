b08dee30ebd24e755a5e742e9a3b6c67ace72f99
ivanrz29 <ivanzou29@gmail.com>
2019-11-11 10:42:41 +0800

    remove the dataset

--- video_image_manipulation/extract_frame.py
+++ video_image_manipulation/extract_frame.py
@@ -2,7 +2,7 @@ import os
 
 def extract_frames(path, filename):
     os.system("mkdir frames/{0}".format(filename[:12]))
-    os.system("ffmpeg -i {0} -f image2 -vf fps=fps=1 frames/{1}/output%d.png".format(os.path.join(path, filename), filename[:12]))
+    os.system("ffmpeg -i {0} -f image2 -vf fps=fps=10 frames/{1}/output%d.png".format(os.path.join(path, filename), filename[:12]))
 
 paths = ['Happy', 'Neutral', 'Sad']
 if __name__ == '__main__':
