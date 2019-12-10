719a6b5d7596272f43b7f8a969e5f62fa18ad941
ivanrz29 <ivanzou29@gmail.com>
2019-12-10 23:37:06 +0800

    modify model

--- models/model.py
+++ models/model.py
@@ -1,8 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 
 
 vgg_arch_16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M5']
@@ -14,40 +11,52 @@ time_step = 16
 input_dim = 3
 output_dim = 1
 
+# With reference to https://hellozhaozheng.github.io/z_post/PyTorch-VGG/
 
 class model(nn.Module):
     def __init__(self, net_arch):
         super(model, self).__init__()
         self.net_arch = net_arch
-        layers = []
+        cnn_layers = []
         in_channels = 3
 
         for arch in net_arch:
             if arch == 'M':
-                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+                cnn_layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
             elif arch == 'M5':
-                layers.append(nn.MaxPool2d(kernel_size=3, stride=1, padding=1))
+                cnn_layers.append(nn.MaxPool2d(kernel_size=3, stride=1, padding=1))
             else:
-                layers.append(nn.Conv2d(in_channels=in_channels, out_channels=arch, kernel_size=3, padding=1))
-                layers.append(nn.ReLU(inplace=True))
+                cnn_layers.append(nn.Conv2d(in_channels=in_channels, out_channels=arch, kernel_size=3, padding=1))
+                cnn_layers.append(nn.ReLU(inplace=True))
                 in_channels=arch
-        self.vgg = nn.ModuleList(layers)
+
+        self.vgg_face = nn.ModuleList(cnn_layers)
+        self.vgg_nose = nn.ModuleList(cnn_layers)
+        self.vgg_mouth = nn.ModuleList(cnn_layers)
+        self.vgg_left_eye = nn.ModuleList(cnn_layers)
+        self.vgg_right_eye = nn.ModuleList(cnn_layers)
+
+        # TODO: define LSTM layers
+        self.lstm_layers = 0
+
+
 
     def forward(self, input_data):
-        for x in input_data:
-            x_face = x[0]
-            x_nose = x[1]
-            x_mouth = x[2]
-            x_left_eye = x[3]
-            x_right_eye = x[4]
+        face = input_data[:][0]
+        nose = input_data[:][1]
+        mouth = input_data[:][2]
+        left_eye = input_data[:][3]
+        right_eye = input_data[:][4]
 
-            for layer in self.vgg:
-                x_face = layer(x_face)
-                x_nose = layer(x_nose)
-                x_mouth = layer(x_mouth)
-                x_left_eye = layer(x_left_eye)
-                x_right_eye = layer(x_right_eye)
-            x = torch.cat((x_face, x_nose, x_mouth, x_left_eye, x_right_eye), dim=2)
+        face = self.vgg_face(face)
+        nose = self.vgg_nose(nose)
+        mouth = self.vgg_mouth(mouth)
+        left_eye = self.vgg_left_eye(left_eye)
+        right_eye = self.vgg_right_eye(right_eye)
 
-        return input_data
+        x = torch.cat((face, nose, mouth, left_eye, right_eye), dim=2)
+
+        # TODO: to feed the concatenated data into LSTM
+        x = 0
+        return x
 
